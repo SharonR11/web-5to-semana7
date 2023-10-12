d760f23bb5b134dc73e5568a7c852de74a66af88
SharonR11 <sharon.rudas@tecsup.edu.pe>
2023-10-12 11:03:37 -0500

    correcion de errores

--- utils/for_testing.js
+++ utils/for_testing.js
@@ -27,10 +27,13 @@ const palindrome =(string) => {
 //pone un promedio 
 const average = array => {
     let sum = 0;
-    // if (!Array.isArray(array)) return 0;
-    // else
-    //     if (array.length ===0)return 0;
-    array.forEach(num => {sum += num});
+    //es un arrreglo?
+    //devolver 0
+    if (!Array.isArray(array)) return 0;
+    //si es 'identicamente' igual a 0 // no 0.000001
+    else
+        if (array.length === 0)return 0;
+    array.forEach(num => { sum += num });
     return sum / array.length;
 }
 
